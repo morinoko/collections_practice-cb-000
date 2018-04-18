8378ea373c86b590ce650918387859987cc24492
Felice Forby <feliceforby@gmail.com>
2018-04-18 22:43:51 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -19,8 +19,11 @@ def swap_elements(array)
   ## array[1], array[2] = array[2], array[1]
 end
 
-def swap_elements_from_to(array, index, destination_index)
-  array[index], array[destination_index] = array[destination_index], array[index]
+def swap_elements_from_to(array, original_index, destination_index)
+  elem_to_move = array[original_index]
+  elem_being_swapped = array[destination_index]
+  array[destination_index] = elem_to_move
+  array[original_index] = elem_being_swapped
 end
 
 def reverse_array(array)
